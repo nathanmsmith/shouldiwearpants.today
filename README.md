d1abd9dbb0c0de2a14e7fa705cd2ddb52b86908e
Nathan Smith <nathan@smith.industries>
2016-09-01 11:41:57 -0700

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# Should-I-Wear-Pants-Today
-The answer to the most pressing question you'll ever face.
+# Should I Wear Pants Today?
+The answer to the most pressing question you'll ever face, now in the form of a website easily accessible from any internet connected device.
+
+# Technologies
+This website would not be possible without the work of much smarter people than me who created all of the libraries and features that are utilized.
+
+* [Flask](http://flask.pocoo.org)
+* [Python Geocoder](https://github.com/DenisCarriere/geocoder)
+* [jQuery](https://jquery.org)
+* [jQuery Geolocation](https://github.com/manuelbieh/jQuery-Geolocation)
+* [Forecast.io Python Wrapper](https://github.com/ZeevG/python-forecast.io)
+
+# License
+All code not otherwise marked is available under the [MIT License](LICENSE).
+
+# Contributors
+* [Nathan Smith](https://github.com/nathunsmitty)
---
+++ static/locate.js
@@ -0,0 +1,93 @@
+$(document).ready(function() {
+    $.geolocation.get({
+        win: determineWhetherPantsShouldBeWornFromLocation,
+        error: showError
+    });
+
+    $('input[type=text]').keyup(function(e) {
+        if (e.keyCode == 13) {
+          determineWhetherPantsShouldBeWornFromUserInput($(this).val());
+        }
+    });
+
+    $('#why').click(function() {
+      areDetailsDown = !areDetailsDown;
+      $("#details").slideToggle();
+    });
+
+    $('#locate').click(function() {
+        $.geolocation.get({
+            win: determineWhetherPantsShouldBeWornFromLocation,
+            error: showError
+        });
+    });
+});
+
+areDetailsDown = false;
+
+function displayLoading() {
+  if (areDetailsDown) {
+    $("#details").slideUp();
+  }
+  $('#answer').hide();
+  $('#loading').show();
+  $('#why').hide();
+  $('#subtext').show();
+  clearInput()
+}
+
+function displayResults(data) {
+  $('#loading').hide();
+  $('#answer').show();
+	$('#answer').text(data.answer);
+  $('#subtext').hide();
+  $('#why').show();
+  $('#details').html(data.details);
+  if (areDetailsDown) {
+    $("#details").slideDown();
+  }
+  clearInput()
+}
+
+function clearInput() {
+  $('input[type=text]').val("");
+  $('input[type=text]').blur();
+}
+
+function determineWhetherPantsShouldBeWornFromUserInput(input) {
+  displayLoading();
+		$.getJSON($SCRIPT_ROOT + '/pant_results_user', {
+				input: input
+		}, displayResults);
+}
+
+function determineWhetherPantsShouldBeWornFromLocation(position) {
+  displayLoading();
+    $.getJSON($SCRIPT_ROOT + '/pant_results_location', {
+        longitude: position.coords.longitude,
+        latitude: position.coords.latitude
+    }, displayResults);
+}
+
+function showError(error) {
+
+    var string = "";
+
+    switch (error.code) {
+        case error.PERMISSION_DENIED:
+            string = "We need to know where you are to check the weather! Please enter your location below.";
+            break;
+        case error.POSITION_UNAVAILABLE:
+            string = "Well this is awkward... Something went wrong! Please try again.";
+            break;
+        case error.TIMEOUT:
+            string = "Well this is awkward... Something went wrong! Please try again.";
+            break;
+        case error.UNKNOWN_ERROR:
+            string = "Well this is awkward... Something went wrong!Please try again.";
+            break;
+    }
+
+    $('#answer').text(string);
+    $('#answer').css("font-size", "2rem");
+}
